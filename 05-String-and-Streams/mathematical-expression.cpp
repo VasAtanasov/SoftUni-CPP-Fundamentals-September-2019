9a7f216185f096f451c2b551e70ac90836498d11
VasAtanasov <vas.atanasov@gmail.com>
2019-10-04 09:10:29 +0300

    added tasks

--- 05-String-and-Streams/mathematical-expression.cpp
+++ 05-String-and-Streams/mathematical-expression.cpp
@@ -2,6 +2,27 @@
 #include <stack>
 
 int main() {
+    size_t count = 0;
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    const size_t SIZE = line.length();
+    for (size_t i = 0; i < SIZE; ++i) {
+        char ch = line[i];
+        if (ch == '(') {
+            count++;
+        } else if (ch == ')') {
+            count--;
+        }
+    }
+
+    if (count == 0) {
+        std::cout << "correct";
+        return 0;
+    }
+
+    std::cout << "incorrect";
 
     return 0;
 }
